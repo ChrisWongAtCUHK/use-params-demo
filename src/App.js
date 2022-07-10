893363027836837c511bf4153e148cbe7034f7b0
Chris Wong <wc38.wong@gmail.com>
2022-07-10 13:26:43 +0800

    Apply useParams

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link
+  Link,
+  useParams
 } from 'react-router-dom';
 
 export default function App() {
@@ -21,6 +22,9 @@ export default function App() {
             <li>
               <Link to="/users">Users</Link>
             </li>
+            <li>
+              <Link to="/portfolio/6">Portfolio</Link>
+            </li>
           </ul>
         </nav>
 
@@ -30,6 +34,9 @@ export default function App() {
           <Route exact path="/" element={<Home />} />
           <Route exact path="/about" element={<About />} />
           <Route exact path="/users" element={<Users />} />
+          <Route path="/portfolio">
+            <Route path=":id" element={<Portfolio />} />
+          </Route>
         </Routes>
       </div>
     </Router>
@@ -47,3 +54,13 @@ function About() {
 function Users() {
   return <h2>Users</h2>;
 }
+
+const Portfolio = () => {
+  let { id } = useParams();
+  return (
+    <div>
+      Portfolio component
+      <p>Url params: {id}</p>
+    </div>
+  );
+};
